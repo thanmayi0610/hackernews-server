4324e209d7394258cb21560d121cabfdf0c374b2
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-03-26 16:30:04 +0530

    refactor: update server initialization and routing structure

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import "dotenv/config"
-import {jwtSecretKey} from '../environment';
-import jwt from "jsonwebtoken";
-const app = new Hono()
+// import { serve } from '@hono/node-server'
+// import { Hono } from 'hono'
+// import "dotenv/config"
+// import {jwtSecretKey} from '../environment';
+// import jwt from "jsonwebtoken";
+// const app = new Hono()
 
-// app.get('/', (c) => {
-//   return c.text('Hello Hono!')
+// // app.get('/', (c) => {
+// //   return c.text('Hello Hono!')
+// // })
+// const payload: jwt.JwtPayload = {
+//   iss: "https://purpleshorts.co.in",
+//   sub: "ThanmayiJR",
+// };
+// const token = jwt.sign(payload, jwtSecretKey, {
+//   algorithm: "HS256",
+//   expiresIn: "30d",
+// });
+// console.log("Token: ",token);
+// try {
+//   const decoded = jwt.verify(token, jwtSecretKey);
+//   console.log("Decoded Payload", decoded);
+// } catch (e) {
+//   console.log("Error", e);
+// }
+// //console.log("jwt secret key",jwtSecretKey);
+// serve({
+//   fetch: app.fetch,
+//   port: 3000
+// }, (info) => {
+//   console.log(`Server is running on http://localhost:${info.port}`)
 // })
-const payload: jwt.JwtPayload = {
-  iss: "https://purpleshorts.co.in",
-  sub: "ThanmayiJR",
-};
-const token = jwt.sign(payload, jwtSecretKey, {
-  algorithm: "HS256",
-  expiresIn: "30d",
+import "dotenv/config";
+import { allRoutes } from "./routes/routes";
+import { serve } from "@hono/node-server";
+
+serve(allRoutes, (info) => {
+  console.log(`Server is running @ http://localhost:${info.port}`);
 });
-console.log("Token: ",token);
-try {
-  const decoded = jwt.verify(token, jwtSecretKey);
-  console.log("Decoded Payload", decoded);
-} catch (e) {
-  console.log("Error", e);
-}
-//console.log("jwt secret key",jwtSecretKey);
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
